7fe26d027000b006d5b8e792f1c007db28e4c0d6
MarinaH18 <kadru-marina@mail.ru>
2024-06-10 09:55:32 +0300

    ДЗ_1. Задание 6 (a, b, c, d,)

---
+++ ДЗ_1.Практика.Знакомство с BI.Герасименко.sql
@@ -0,0 +1,11 @@
+select * from Sales.SalesTerritory
+
+select TerritoryID, Name
+From Sales.SalesTerritory
+
+select * From Person.Person
+where LastName='Norman'
+
+select * from Person.Person
+where EmailPromotion <> 2
+
